4ec50b2c1b67912d9e055dcba840f50afbf31164
djason1337 <156970060+djason1337@users.noreply.github.com>
2024-02-05 17:05:17 -0600

    Delete homework1Spring/1.12.py

--- homework1Spring/1.12.py
+++
@@ -1,7 +0,0 @@
-#Dominic Jason 2203959
-
-userNum = int(input())
-userNumSquared = userNum * userNum # Bug here; fix it when instructed
-
-
-print(userNumSquared) # Output formatting issue here; fix it when instructed
