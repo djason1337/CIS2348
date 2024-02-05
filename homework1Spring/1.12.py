ed4698f8b5c8e556579453ea9e88a512c2f42073
djason1337 <156970060+djason1337@users.noreply.github.com>
2024-02-05 17:05:32 -0600

    Add files via upload

---
+++ homework1Spring/1.12.py
@@ -0,0 +1,7 @@
+# Dominic Jason 2203959
+
+userNum = int(input())
+userNumSquared = userNum * userNum  # Bug here; fix it when instructed
+
+
+print(userNumSquared)  # Output formatting issue here; fix it when instructed
