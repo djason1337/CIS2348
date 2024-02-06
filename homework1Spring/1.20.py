6fecc451ceb519fff954811ac76753de37cf1e80
djason1337 <156970060+djason1337@users.noreply.github.com>
2024-02-06 12:28:56 -0600

    Add files via upload

---
+++ homework1Spring/1.20.py
@@ -0,0 +1,14 @@
+# Dominic Jason 2203959
+
+user_num = int(input('Enter integer:\n'))
+print('You entered:',user_num)
+num_square = user_num * user_num
+print(f'{user_num} squared is {num_square}')
+num_cubed = user_num * user_num * user_num
+print(f'And {user_num} cubed is {num_cubed} !!')
+user_num2 = int(input('Enter another integer:\n'))
+num_add = user_num + user_num2
+num_mult = user_num * user_num2
+print(f'{user_num} + {user_num2} is {num_add}')
+print(f'{user_num} * {user_num2} is {num_mult}')
+
